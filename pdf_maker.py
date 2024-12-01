79570762a9514cbd25bd72a8998f5bc67eb6804c
obmbane <obmbaneaws@gmail.com>
2024-12-01 02:28:03 +0200

    Created PDF Pages from CSV File

--- pdf_maker.py
+++ pdf_maker.py
@@ -4,14 +4,17 @@ import pandas as pd
 pdf = FPDF(orientation='P',unit='mm',format='A4')
 
 df =pd.read_csv('topics_pdf_gen.csv')
-print (df)
+#print (df)
 
 for index, row in df.iterrows():
-    num_pages = int(row['Pages'])
+    pdf.add_page()
+    pdf.set_font(family='Times',style='B',size=20)
+    pdf.set_text_color(0,0,255)
+    pdf.cell(w=10, h=12, txt=row['Topic'], align='L', ln=1, border=0)
+    pdf.line(10,21,200,21)
 
-    for i in range (num_pages):
+    for i in range (row['Pages']-1):
         pdf.add_page()
-        pdf.set_font(family='Times',style='B',size=20)
-        pdf.cell(w=10, h=12, txt=row['Topic'], align='L', ln=1, border=0)
+
 
 pdf.output('output.pdf')
